a13a6c5cc9369dae024686073c8a29c630ab4b11
IlyaKuznetsov1983 <6370303@mail.ru>
2024-05-20 18:01:14 +0300

    check

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,10 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
 //        System.out.println("Hello Ilya Sergeevich!");
-//        String name = "Илья";
-
-//        printThreeWords();
-//        public static void printThreeWords() {
-//            System.out.println("Orange");
+//
         }
     }
