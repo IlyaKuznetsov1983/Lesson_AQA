9e823e29a9b7e683dd3a4dfaf2e67d94ebacc556
IlyaKuznetsov1983 <6370303@mail.ru>
2024-05-20 21:18:46 +0300

    numb

---
+++ untitled/src/Numb.java
@@ -0,0 +1,14 @@
+public class Numb {
+    public static void main(String [] args) {
+        compareNumbers();
+    }
+    public static void compareNumbers() {
+        int a = 30;
+        int b = 30;
+        if (a>=b) {
+            System.out.println("a>=b");
+        } if (a<b) {
+            System.out.println("a>b");
+        }
+    }
+}
