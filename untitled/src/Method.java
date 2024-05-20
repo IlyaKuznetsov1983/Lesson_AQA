df725519ffe3a3d457433571e0301d9966dd20d9
IlyaKuznetsov1983 <6370303@mail.ru>
2024-05-20 23:34:31 +0300

    meth

---
+++ untitled/src/Method.java
@@ -0,0 +1,17 @@
+
+public class Method {
+    public static void main(String[] args) {
+
+
+        System.out.println(meth5(9, 8));
+
+    }
+
+    static boolean meth5(double a, double b) {
+        return (a + b >= 10) && (a + b <= 20);
+    }
+
+
+}
+
+
