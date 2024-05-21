493a8c12829ecd454e87ffe5562ff13b61eefe4c
IlyaKuznetsov1983 <6370303@mail.ru>
2024-05-21 14:42:44 +0300

    koo

---
+++ untitled/src/Method7.java
@@ -0,0 +1,13 @@
+public class Method7 {
+    public static void main(String[] args) {
+
+
+        System.out.println(meth7 (10));
+
+    }
+    static boolean meth7(int a) {
+        return a <= 0;
+    }
+
+
+}
---
+++ untitled/src/Method8.java
@@ -0,0 +1,14 @@
+public class Method8 {
+    public static void main(String[] args) {
+
+
+        meth8 ("три раза - КУ!!!", 3);
+
+    }
+
+    public static void meth8(String message, int b) {
+        for (int k = 0; k < b; k++) {
+            System.out.println(message);
+        }
+    }
+}
