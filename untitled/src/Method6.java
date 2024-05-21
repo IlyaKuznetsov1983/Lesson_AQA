b0b7c6487dcc2729a7bbe445b4cdfcd00506ecbe
IlyaKuznetsov1983 <6370303@mail.ru>
2024-05-21 14:10:39 +0300

    meth6

---
+++ untitled/src/Method6.java
@@ -0,0 +1,13 @@
+public class Method6 {
+    public static void main(String[] args) {
+
+
+        meth6(-1983);
+
+    }
+    static void meth6(int a) {
+        System.out.println((a >= 0) ? "Положительное" : "Отрицательное");
+    }
+
+
+}
