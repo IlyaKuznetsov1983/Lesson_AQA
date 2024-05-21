ef73bd7659616d819c87ec85bbd41e5c991d6804
IlyaKuznetsov1983 <6370303@mail.ru>
2024-05-21 15:23:38 +0300

    years

---
+++ untitled/src/Method9.java
@@ -0,0 +1,10 @@
+public class Method9 {
+    public static void main(String[] args) {
+        System.out.println(meth9 (2024));
+
+    }
+    static boolean meth9(int year) {
+        return (((year % 4 == 0) && !(year % 100 == 0)) || (year % 400 == 0));
+        }
+    }
+
