cd7d8b4e27532a873a714ab418e6f5c0ca4a0739
IlyaKuznetsov1983 <6370303@mail.ru>
2024-05-28 13:48:27 +0300

    persons4

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ public class Main {
                         new Persons("Efremov", "Director","ivan@mail.ru", 987654321 , 100500, 47),
                         new Persons("Castaneda", "Manager","carlos@gmail.com", 123456789,334455,33),
                         new Persons("Strugatsky", "Postman" , "strugatkybrothers@mail.ru", 99988877,1010101,53),
-                        new Persons("Glukhovsky", "", "metro2033@mail.ru", 77777777, 1000000, 27),
-                        new Persons("Pelevin", "Doctor", "sigmundinacage@mail.ru", 3030303, 999000, 49)};
+                        new Persons("Glukhovsky", "Writer", "metro2033@mail.ru", 77777777, 1000000, 27),
+                        new Persons("Pelevin", "Doctor", "sigmundinacafe@mail.ru", 3030303, 999000, 49)};
 
         for(int i = 0; i < persons.length; ++i) {
             if (persons[i].getAge() > 40) {
