81fb495e9ee6006fa927c9dd59ea743787e7c329
IlyaKuznetsov1983 <6370303@mail.ru>
2024-05-28 13:24:37 +0300

    persons2

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Persons[] persons =
                 new Persons[]{
-                        new Persons("Efremov", "ivan@mail.ru", 47),
-                        new Persons("Castaneda", "carlos@gmail.com", 33),
-                        new Persons("Strugatsky", "strugatkybrothers@mail.ru", 53),
-                        new Persons("Glukhovsky", "metro2033@mail.ru", 27),
-                        new Persons("Pelevin", "sigmundinacage@mail.ru", 49)};
+                        new Persons("Efremov", "Director","ivan@mail.ru", 987654321 , 100500, 47),
+                        new Persons("Castaneda", "Manager","carlos@gmail.com", 123456789,334455,33),
+                        new Persons("Strugatsky", "Postman" , "strugatkybrothers@mail.ru", 99988877,1010101,53),
+                        new Persons("Glukhovsky", "", "metro2033@mail.ru", 77777777, 1000000, 27),
+                        new Persons("Pelevin", "Doctor", "sigmundinacage@mail.ru", 3030303, 999000, 49)};
 
         for(int i = 0; i < persons.length; ++i) {
             if (persons[i].getAge() > 40) {
--- src/Persons.java
+++ src/Persons.java
@@ -1,19 +1,25 @@
 public class Persons {
     private String fio;
+    private String post;
     private int age;
+    private int telephone;
+    private int cash;
     private String email;
 
     public int getAge() {
         return this.age;
     }
 
-    public Persons(String fio, String email, int age) {
+    public Persons(String fio,String post, String email, int telephone, int cash, int age) {
         this.fio = fio;
+        this.post = post;
         this.email = email;
+        this.telephone = telephone;
+        this.cash = cash;
         this.age = age;
     }
 
     public void printInfo() {
-        System.out.println("fio" + " " + this.fio + " " + "email" + " " + this.email + "age" + " " + this.age);
+        System.out.println("fio" + " " + this.fio + "post" + " " + this.post + " " + "email" + " " + this.email + " " + "age" + " " + this.age);
     }
 }
