5a7a3f807407966ed3f23c9111a955d80fd747c3
IlyaKuznetsov1983 <6370303@mail.ru>
2024-06-13 22:32:49 +0300

    dogAdded

--- src/Animals.java
+++ src/Animals.java
@@ -2,7 +2,10 @@ public abstract class Animals {
     private static int animalsCount = 0;
      String name;
 
-    public Animals(); {
-        animalscount++;
+    public Animals() {
+        animalsCount++;
     }
+
+    protected abstract void running (int distance);
+    protected abstract void swimming (int distance);
 }
---
+++ src/Dog.java
@@ -0,0 +1,23 @@
+public class Dog extends Animals {
+    private static int dogCount = 0;
+
+    public Dog() {
+        dogCount++;
+    }
+
+
+    @Override
+
+    public void run(int distance) {
+        if (distance >= 0 && distance <= 500) {
+            System.out.println("Собака бежит " + distance + " метров");
+        } else System.out.println("Собака не хочет");
+    }
+
+    @Override
+    public void swim(int distance) {
+        if (distance >= 0 && distance <= 10) {
+            System.out.println("Собака плывет " + distance + " метров");
+        } else System.out.println("Собака устал");
+    }
+}
