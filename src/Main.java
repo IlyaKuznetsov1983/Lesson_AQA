83dd4c7318e0de88b366f7cc4423bdd96b32e20a
IlyaKuznetsov1983 <6370303@mail.ru>
2024-06-20 21:50:00 +0300

    addedBrands

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
-    }
-}
+        String[] strings = {
+                "samsung",
+                "honor",
+                "nokia",
+                "vivo",
+                "xiaomi",
+                "oppo",
+                "redmi",
+                "sony ericsson",
+                "tcl",
+                "siemens",
+                "vertu",
+                "yota",
+                "BQ",
+                "cube",
+                "asus",
+                "3Q",
+                "BBK",
+                "honor",
+                "vivo",
+                "redmi",
+                "honor",
+                "nokia"
+        };
