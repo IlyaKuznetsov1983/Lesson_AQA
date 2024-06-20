c62e15f7342929957094e1e99a7d723a642128df
IlyaKuznetsov1983 <6370303@mail.ru>
2024-06-20 22:38:33 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -26,3 +26,33 @@ public class Main {
                 "honor",
                 "nokia"
         };
+
+        Set<String> uniqueBrands = new HashSet<>(Arrays.asList(strings));
+        System.out.println(uniqueBrands);
+
+        Map<String, Integer> duplicateInfo = new HashMap<>();
+
+        for(String brand : strings) {
+            if (duplicateInfo.containsKey(brand)) {
+                duplicateInfo.put(brand, duplicateInfo.get(brand) + 1);
+            } else {
+                duplicateInfo.put(brand, 1);
+            }
+        }
+
+        System.out.println(duplicateInfo);
+
+        System.out.println("..............................");
+
+        PhoneDirectory.addNumberPhone("Панин", "Алексей","+79533551109");
+        PhoneDirectory.addNumberPhone("Монгол", "Олег", "+79312543879");
+        PhoneDirectory.addNumberPhone("Кологривый", "Никита","+79455557687");
+        PhoneDirectory.addNumberPhone("Ивлев", "Павел", "+79477777555");
+        PhoneDirectory.addNumberPhone("Джигурда", "Никита", "+79212773888");
+        PhoneDirectory.addNumberPhone("Хасбулла", "Магомедов","+79476543989");
+        PhoneDirectory.addNumberPhone("Харламов", "Игорь", "+79477987654");
+
+        System.out.println(PhoneDirectory.getNumberPhone("Кологривый"));
+        System.out.println("..............................");
+    }
+}
--- src/PhoneDirectory.java
+++ src/PhoneDirectory.java
@@ -1,2 +1,26 @@
-public class PhoneDirectory {
-}
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+
+    public class PhoneDirectory {
+        static private Map<String, List<String>> listOfAbonents = new HashMap<>();
+
+        public static List<String> getNumberPhone(String lastName) {
+            return listOfAbonents.get(lastName);
+        }
+
+        public static void addNumberPhone(String lastName, String firstName, String numberPhone) {
+            if (listOfAbonents.containsKey(lastName)) {
+                List<String> numbersPhone = listOfAbonents.get(lastName);
+                numbersPhone.add(String.format("%s %s - %s", lastName, firstName, numberPhone));
+            } else {
+                List<String> numbersPhone = new ArrayList<>();
+                numbersPhone.add(String.format("%s %s - %s", lastName, firstName, numberPhone));
+
+                listOfAbonents.put(lastName, numbersPhone);
+            }
+        }
+    }
+
