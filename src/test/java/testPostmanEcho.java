73976f6b04b1cc3d468594dc1e0219b5a2d51720
IlyaKuznetsov1983 <6370303@mail.ru>
2024-07-04 19:56:56 +0300

    add

--- src/test/java/testPostmanEcho.java
+++ src/test/java/testPostmanEcho.java
@@ -1,8 +1,22 @@
-import io.restassured.response.Response;
 import org.junit.Test;
-
+import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class testPostmanEcho {
+    @Test
+    public void testGetRequest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .queryParam("foo1", "bar1")
+                .queryParam("foo2", "bar2")
+                .when()
+                .get("/get")
+                .then()
+                .log().all()
+                .assertThat()
+                .body("args.foo1", equalTo("bar1"))
+                .body("args.foo2", equalTo("bar2"));
+    }
+
 }
