f7be0747e2cd8196e308929c2e7ddd5742ec0c78
IlyaKuznetsov1983 <6370303@mail.ru>
2024-07-04 19:59:15 +0300

    added

--- src/test/java/testPostmanEcho.java
+++ src/test/java/testPostmanEcho.java
@@ -19,4 +19,28 @@ public class testPostmanEcho {
                 .body("args.foo2", equalTo("bar2"));
     }
 
+    @Test
+    public void testPostRawText() {
+        Response response = given()
+                .log().all()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain")
+                .body("value")
+                .when()
+                .post("/post")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("data", equalTo("value"))
+                .body("headers.host", equalTo("postman-echo.com"))
+                .body("headers.x-request-start", notNullValue())
+                .body("headers.x-forwarded-proto", equalTo("https"))
+                .body("headers.x-forwarded-port", equalTo("443"))
+                .body("headers.x-amzn-trace-id", notNullValue())
+                .body("headers.accept", equalTo("*/*"))
+                .body("headers.postman-token", nullValue())
+                .body("headers.accept-encoding", equalTo("gzip,deflate"))
+                .body("headers.cookie", nullValue())
+                .extract().response();
+    }
 }
