3889295b27d9765eb9d6828646234c4a0c285f9f
IlyaKuznetsov1983 <6370303@mail.ru>
2024-07-04 19:35:06 +0300

    init

---
+++ src/test/java/testPostmanEcho.java
@@ -0,0 +1,8 @@
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class testPostmanEcho {
+}
